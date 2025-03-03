e3f755d68db3a6b63ae132ba3c0ef908cc3a1d44
Ayoub-khedher <ayoubgustavo2025@gmail.com>
2025-03-03 12:02:23 +0100

    car shop

--- src/app/car-parts/car-parts.component.ts
+++ src/app/car-parts/car-parts.component.ts
@@ -10,26 +10,26 @@ import { CommonModule , NgForOf} from '@angular/common';
 })
 export class CarPartsComponent {
   parts = [
-    { name: 'Pièces moteur', image: 'assets/images/engine.jpg' },
-    { name: 'Direction / Suspension / Train', image: 'assets/images/suspension.jpg' },
+    { name: 'Pièces moteur', image: 'assets/Capture_d_écran_2025-03-02_222914-removebg-preview.png' },
+    { name: 'Direction / Suspension / Train', image: 'assets/direction-removebg-preview.png' },
     { name: 'Filtres et huile', image: 'assets/filtre-removebg-preview.png' },
-    { name: 'Optiques / Phares / Ampoules', image: 'assets/images/lights.jpg' },
-    { name: 'Démarrage électrique', image: 'assets/images/electric.jpg' },
-    { name: 'Freinage', image: 'assets/images/brakes.jpg' },
-    { name: 'Essuie-glaces et pièces', image: 'assets/images/wipers.jpg' },
-    { name: 'Embrayage et Boîte de vitesse', image: 'assets/images/clutch.jpg' },
-    { name: 'Distribution et Accessoires', image: 'assets/images/distribution.jpg' },
-    { name: 'Échappement', image: 'assets/images/exhaust.jpg' },
-    { name: 'Pièces Habitacle', image: 'assets/images/interior.jpg' },
-    { name: 'Capteurs et Sondes', image: 'assets/images/sensors.jpg' },
-    { name: 'Carrosserie / Vitres / Peinture', image: 'assets/images/bodywork.jpg' },
-    { name: 'Joints et Étanchéité', image: 'assets/images/seals.jpg' },
-    { name: 'Pneus et Équipements Roue', image: 'assets/images/tires.jpg' },
-    { name: 'Outillage', image: 'assets/images/tools.jpg' },
-    { name: 'Chauffage et Climatisation', image: 'assets/images/ac.jpg' },
-    { name: 'Accessoires et Équipements', image: 'assets/images/accessories.jpg' },
-    { name: 'Attelage et Portage', image: 'assets/images/trailer.jpg' },
-    { name: 'Entretien et Nettoyage', image: 'assets/images/cleaning.jpg' }
+    { name: 'Optiques / Phares / Ampoules', image: 'assets/Capture_d_écran_2025-03-02_222950-removebg-preview.png' },
+    { name: 'Démarrage électrique', image: ' assets/Capture_d_écran_2025-03-02_223511-removebg-preview (1).png' },
+    { name: 'Freinage', image: 'assets/frin-removebg-preview.png' },
+    { name: 'Essuie-glaces et pièces', image: 'assets/téléchargé (1).jpg' },
+    { name: 'Embrayage et Boîte de vitesse', image: 'assets/Capture_d_écran_2025-03-02_223317-removebg-preview.png' },
+    { name: 'Distribution et Accessoires', image: 'assets/téléchargé (2).jpg' },
+    { name: 'Échappement', image: 'assets/centre-auto-guichen-dap35-echappement-voiture-001.jpg' },
+    { name: 'Pièces Habitacle', image: 'assets/images.jpg' },
+    { name: 'Capteurs et Sondes', image: 'assets/Capture_d_écran_2025-03-02_223435-removebg-preview.png' },
+    { name: 'Carrosserie / Vitres / Peinture', image: 'assets/Capture_d_écran_2025-03-02_223129-removebg-preview.png' },
+    { name: 'Joints et Étanchéité', image: 'assets/IMG-Seal_Solutions.webp' },
+    { name: 'Pneus et Équipements Roue', image: 'assets/téléchargé (3).jpg' },
+    { name: 'Outillage', image: 'assets/images (1).jpg' },
+    { name: 'Chauffage et Climatisation', image: 'assets/Capture_d_écran_2025-03-02_223154-removebg-preview.png' },
+    { name: 'Accessoires et Équipements', image: 'assets/téléchargé.jpg' },
+    { name: 'Attelage et Portage', image: 'assets/thumb_16554_category_large.webp' },
+    { name: 'Entretien et Nettoyage', image: 'assets/Capture_d_écran_2025-03-02_223347-removebg-preview.png' }
   ];
     constructor(private router: Router) { }
   
